7133fbf7dedbdabda336d31ce02ee4d8f6b55962
naeem majumder <naeemmajumder00@gmail.com>
2024-12-10 23:41:28 +0600

    add user registration page

--- src/components/header/AdminNav.jsx
+++ src/components/header/AdminNav.jsx
@@ -76,6 +76,7 @@ const AdminNav = () => {
                 tabIndex={0}
                 className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 shadow"
               >
+                
                 <div className="card-body">
                   <span className="text-lg font-bold">Naeem Majumder</span>
                   <span className="text-info">naeemmajumder00@gmail.com</span>
